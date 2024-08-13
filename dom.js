6d9f5e56c9ceb865482daeb64e60bc345c0595e4
error-code-red <149626268+error-code-red@users.noreply.github.com>
2024-08-13 04:52:52 +0000

    Final Touches

--- dom.js
+++ dom.js
@@ -5,8 +5,7 @@ function updateCounter() {
     
     pressed = pressed + 1;
     document.getElementById("counter").innerHTML = pressed;
-    console
-    
+ 
 }
 
 myButton.addEventListener('click', updateCounter)
